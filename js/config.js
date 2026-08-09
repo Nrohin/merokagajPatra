b6848eff7c687c8ea2279a09eeb5e8a2611579b8
Nrohin <hoissema@gmail.com>
2026-08-09 21:41:31 +0545

    update

--- js/config.js
+++ js/config.js
@@ -14,8 +14,8 @@ if (typeof window !== 'undefined' && window.MEROKAGAJ_CONFIG) {
 
 export const CONFIG = Object.assign({
   ADMIN_PATH: 'manage-portal-x7k9',
-  SUPABASE_URL: 'https://txwrsqictjfxcxhwjmtj.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4d3JzcWljdGpmeGN4aHdqbXRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyODQ0NTQsImV4cCI6MjEwMTg2MDQ1NH0.z3komc6qS1zjnHgDKjYAJQNPQCAGtIodjk7tbLhnWi4',
+  SUPABASE_URL: 'https://tztzxxujmisidlhftuwp.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6dHp4eHVqbWlzaWRsaGZ0dXdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyODY3NTIsImV4cCI6MjEwMTg2Mjc1Mn0.4QlrnsClkEWWlq6ApFXmtRG9hjWJFjr1ITSwszl9A-w',
   SITE_NAME_EN: 'MeroKagajPatra',
   SITE_NAME_NE: 'मेरोकागजपत्र',
 }, localOverrides);
